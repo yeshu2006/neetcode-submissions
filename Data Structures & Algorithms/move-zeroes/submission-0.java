510e54eff954e92154fb30daf744ea85847196ac
Yeshwanth <yeshwanth3006@gmail.com>
2026-08-17 11:14:33 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                swap(nums,i,j);
+                j++;
+            }
+        }
+    }
+}
